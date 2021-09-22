2236ab07de3af071daea4e4e04fc5098c10eed75
Camelia Boban <aissatech@users.noreply.github.com>
2021-09-22 23:36:28 +0200

    Cretae WikidataQueryVisualisationApplication.java

---
+++ src/main/java/it/wikidonne/wikidata/WikidataQueryVisualisationApplication.java
@@ -0,0 +1,13 @@
+package it.wikidonne.wikidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikidataQueryVisualisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikidataQueryVisualisationApplication.class, args);
+	}
+
+}
